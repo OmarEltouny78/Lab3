e7a0618c8244522dd3566659cead264d923dbf23
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2022-10-21 13:20:58 +0200

    Lab3

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+	\\ID,firstName,lastName,salary are the variables
+    
+
+    \\write the constructor
+	
+	\\getAnnualSalary function. Salary varaible is salary per month
+	
+	\\raiseSalary by percent
+   
+
+    
+
+	\\write the setters and getters
+}
---
+++ ImprovedDog.java
@@ -0,0 +1,13 @@
+public class ImprovedDog {
+    private String breed;
+    private int age;
+    private String color;
+
+    public ImprovedDog(String breed, int age, String color) {
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    \\write setters and getters and make sure that the age does not exceed 10
+}
